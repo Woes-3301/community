1f5385546096f329c3bf210923c40219e2cb076c
Woes-3301 <3171310977@qq.com>
2022-12-14 17:11:28 +0800

    12/14 发送私信

--- src/main/java/com/nowcoder/community/dao/MessageMapper.java
+++ src/main/java/com/nowcoder/community/dao/MessageMapper.java
@@ -12,13 +12,24 @@ import java.util.List;
 @Mapper
 public interface MessageMapper {
 
+    //查询当前用户的私信列表，针对每个会话返回最新的一条私信
     List<Message> selectConversations(int userId, int offset, int limit);
 
+    //查询当前用户的私信数量
     int selectConversationCount(int userId);
 
+    //查询某个会话包含的私信（列表
     List<Message> selectLetters(String conversationId, int offset, int limit);
 
+    //某个会话包含的私信数量
     int selectLetterCount(String conversationId);
 
+    //未读私信的数量
     int selectLetterUnreadCount(int userId, String conversationId);
+
+    // 新增消息
+    int insertMessage(Message message);
+
+    //修改私信的状态status
+    int updateStatus(List<Integer> ids, int status);
 }
--- src/main/resources/static/js/letter.js
+++ src/main/resources/static/js/letter.js
@@ -4,11 +4,28 @@ $(function(){
 });
 
 function send_letter() {
-	$("#sendModal").modal("hide");
-	$("#hintModal").modal("show");
-	setTimeout(function(){
-		$("#hintModal").modal("hide");
-	}, 2000);
+	$("#sendModal").modal("hide");//关闭弹出框
+
+	var toName = $("#recipient-name").val();
+	var content = $("#message-text").val();
+	$.post(
+		CONTEXT_PATH + "/letter/send",
+		{"toName":toName,"content":content},
+		function (data){
+			data = $.parseJSON(data);
+			if(data.code == 0){
+				$("#hintBody").text("发送成功");
+			}else{
+				$("#hintBody").text(data.msg);
+			}
+
+			$("#hintModal").modal("show");//打开提示框
+			setTimeout(function(){
+				$("#hintModal").modal("hide");
+				location.reload();//刷新页面
+			}, 2000);
+		}
+	);
 }
 
 function delete_msg() {
